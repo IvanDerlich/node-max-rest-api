7f4226debeaeb42051035180f42ef08de0585ba1
Ivan Derlich <a@ivanderlich.com>
2024-10-13 21:03:20 -0300

    Remove excess code

--- be/app.js
+++ be/app.js
@@ -17,7 +17,6 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
